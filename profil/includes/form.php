62b90f5539eab6fcf586d307d66504007e05923d
JPcookie290 <julia.paier@yahoo.com>
2024-05-22 13:52:26 +0200

    end of day

--- profil/includes/form.php
+++ profil/includes/form.php
@@ -12,7 +12,7 @@ $profil = [
     'street' => '',
     'postcode' => '',
     'city' => '',
-    'county' => '',
+    'country' => '',
     'image' => '',
 ];
 
@@ -26,7 +26,7 @@ $errors = [
     'street' => '',
     'postcode' => '',
     'city' => '',
-    'county' => '',
+    'country' => '',
     'image' => '',
 ];
 $max_file_size = 1024 * 1024 * 4; 
@@ -54,51 +54,58 @@ if ( $_SERVER['REQUEST_METHOD' ] === 'POST' ) {
 <main style="padding-top: 2rem">
     <h3>Input Personal Data</h3>
     <form action="form.php" method="post">
-        <label for="gender">Gender
-            <input type="checkbox" name="male" id="male" value="male">
-            <input type="checkbox" name="female" id="female" value="female">
-            <input type="checkbox" name="divers" id="divers" value="divers">
-            <span style="color: red">><?= $errors['gender']; ?></span>
-        </label>
+        Gender
+        <div style="display: flex">
+            <label for="male" >male
+                <input type="checkbox" name="male" id="male" value="male">
+            </label>
+            <label for="female">female
+                <input type="checkbox" name="female" id="female" value="female">
+            </label>
+            <label for="divers">divers
+                <input type="checkbox" name="divers" id="divers" value="divers">
+            </label>
+            <span style="color: red"><?= $errors['gender']; ?></span>
+        </div>
         <label for="firstname">
             <input type="text" name="firstname" id="firstname" placeholder="First Name" value="<?= e( (string) $profil['firstname'] )?>">
             <span style="color: red"><?= $errors['firstname']; ?></span>
         </label>
         <label for="lastname">
             <input type="text" name="lastname" id="lastname" placeholder="Last Name" value="<?= e( (string) $profil['lastname'] )?>">
-            <span style="color: red">><?= $errors['lastname']; ?></span>
+            <span style="color: red"><?= $errors['lastname']; ?></span>
         </label>
         <label for="age">
             <input type="text" name="age" id="age" placeholder="Age" value="<?= e( (string) $profil['age'] )?>">
-            <span style="color: red">><?= $errors['age']; ?></span>
+            <span style="color: red"><?= $errors['age']; ?></span>
         </label>
         <label for="email">
             <input type="email" name="email" id="email" placeholder="E-Mail" value="<?= e( (string) $profil['email'] )?>">
-            <span style="color: red">><?= $errors['email']; ?></span>
+            <span style="color: red"><?= $errors['email']; ?></span>
         </label>
         <label for="phone">
             <input type="text" name="phone" id="phone" placeholder="Phone Number" value="<?= e( (string) $profil['phone'] )?>">
-            <span style="color: red">><?= $errors['phone']; ?></span>
+            <span style="color: red"><?= $errors['phone']; ?></span>
         </label>
         <label for="street">
             <input type="text" name="street" id="street" placeholder="Street" value="<?= e( (string) $profil['street'] )?>">
-            <span style="color: red">><?= $errors['street']; ?></span>
+            <span style="color: red"><?= $errors['street']; ?></span>
         </label>
         <label for="postcode">
             <input type="text" name="postcode" id="postcode" placeholder="Postcode" value="<?= e( (string) $profil['postcode'] )?>">
-            <span style="color: red">><?= $errors['postcode']; ?></span>
+            <span style="color: red"><?= $errors['postcode']; ?></span>
         </label>
         <label for="city">
             <input type="text" name="city" id="city" placeholder="City" value="<?= e( (string) $profil['city'] )?>">
-            <span style="color: red">><?= $errors['city']; ?></span>
+            <span style="color: red"><?= $errors['city']; ?></span>
         </label>
         <label for="country">
             <input type="text" name="country" id="country" placeholder="Country" value="<?= e( (string) $profil['country'] )?>">
-            <span style="color: red">><?= $errors['country']; ?></span>
+            <span style="color: red"><?= $errors['country']; ?></span>
         </label>
         <label for="image">Upload Profile Picture:
             <input type="file" name="image" id="image" accept="image/*">
-            <span style="color: red">><?= $errors['image']; ?></span>
+            <span style="color: red"><?= $errors['image']; ?></span>
         </label>
         <input type="submit" value="Save">
     </form>
